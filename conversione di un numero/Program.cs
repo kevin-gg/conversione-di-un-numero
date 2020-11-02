f9a018b619bf4e69f521efb927db836cd205dd70
kevin trentini <trentinikevin6@gmail.com>
2020-11-02 18:54:13 +0100

    Add project files.

---
+++ conversione di un numero/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace conversione_di_un_numero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int baseP, baseF;
+            double numero;
+            Console.Write("Inserisci la base da cui convertire: ");
+            baseP = int.Parse(Console.ReadLine());
+            Console.Write("Inserisci la base in cui convertire: ");
+            baseF = int.Parse(Console.ReadLine());
+            Console.Write("inserisci il numero da convertire: ");
+            numero = double.Parse(Console.ReadLine());
+            string risultato = Convert.ToString(Convert.ToInt32(numero, baseP), baseF);
+            Console.WriteLine($"il numero convertito è {risultato}");
+        }
+    }
+}
