828e11664432fcc5e81510c5c15fd875c70dfdb1
kevin trentini <trentinikevin6@gmail.com>
2020-11-09 16:37:59 +0100

    Modifica

--- conversione di un numero/Program.cs
+++ conversione di un numero/Program.cs
@@ -7,11 +7,11 @@ namespace conversione_di_un_numero
         static void Main(string[] args)
         {
             int numero, baseF;
-           Console.Write("inserisci il numero da convertire: ");
+            Console.Write("inserisci il numero da convertire: ");
             numero = int.Parse(Console.ReadLine());
             Console.Write("Inserisci la base in cui convertire: ");
             baseF = int.Parse(Console.ReadLine());
-            string risultato = Convert.ToString(numero, baseF);
+            string risultato = Convert.ToString(numero, baseF).PadLeft(8,'8');
             Console.WriteLine($"il numero convertito è {risultato}");
         }
     }
